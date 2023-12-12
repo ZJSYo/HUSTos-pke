fb7cae02e2882167291dced44d5e104b11daee69
zhangjinshen <1658541663@qq.com>
2023-12-12 20:21:25 +0800

    my work on lab4_1 is done.

--- kernel/rfs.c
+++ kernel/rfs.c
@@ -474,7 +474,7 @@ struct vinode *rfs_create(struct vinode *parent, struct dentry *sub_dentry) {
   }
 
   if (free_dinode == NULL)
-    panic("rfs_create: no more free disk inode, we cannot create file.\n" );
+//    panic("rfs_create: no more free disk inode, we cannot create file.\n" );
 
   // initialize the states of the file being created
 
@@ -486,8 +486,11 @@ struct vinode *rfs_create(struct vinode *parent, struct dentry *sub_dentry) {
   // nlinks, i.e., the number of links.
   // blocks, i.e., its block count.
   // Note: DO NOT DELETE CODE BELOW PANIC.
-  panic("You need to implement the code of populating a disk inode in lab4_1.\n" );
-
+//  panic("You need to implement the code of populating a disk inode in lab4_1.\n" );
+  free_dinode->size = 0;
+  free_dinode->type = R_FILE;
+  free_dinode->nlinks = 1;
+  free_dinode->blocks = 100;
   // DO NOT REMOVE ANY CODE BELOW.
   // allocate a free block for the file
   free_dinode->addrs[0] = rfs_alloc_block(parent->sb);
