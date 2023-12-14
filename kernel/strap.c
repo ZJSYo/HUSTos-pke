adb3d8ed1bae10f5b9757883f60e08e178a94f01
zhangjinshen <1658541663@qq.com>
2023-12-14 14:33:50 +0800

    my work on lab2_challenge_1 is done.

--- kernel/strap.c
+++ kernel/strap.c
@@ -59,11 +59,19 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
             // dynamically increase application stack.
             // hint: first allocate a new physical page, and then, maps the new page to the
             // virtual address that causes the page fault.
-            ;
+        {
+            if(stval < current->trapframe->regs.sp)
+            {
+                panic("this address is not available!");
+                break;
+            }
             void *pa = alloc_page();
             user_vm_map(current->pagetable, stval / (PGSIZE) * (PGSIZE), PGSIZE, (uint64)(pa),
                         prot_to_type(PROT_WRITE | PROT_READ, 1));
             break;
+        }
+
+
         default:
             sprint("unknown page fault.\n");
             break;
